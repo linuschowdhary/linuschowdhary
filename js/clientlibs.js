1c0f1c2a5b54ec227514b4985b1bed958f2858e3
Sunil Chowdhary <sunil_chowdhary@outlook.com>
2016-06-20 00:04:22 +0100

    2more pages

--- js/clientlibs.js
+++ js/clientlibs.js
@@ -1,8 +1,12 @@
-$("ul.nav li.ajaxload").click(function() {
-  $.get("//www.sunilchowdhary.com" +    $(this).children("a").attr("data-load"), function(data) {
+$( function() {
+  $("ul.nav li.ajaxload").click(function() {
+  $.get($(this).children("a").attr("data-load"), function(data) {
     $('div.content').fadeOut('slow', function() {
       $("div.content").html(data);
       $('div.content').fadeIn('slow');
     });
   });
 });
+
+  $( '#gallery' ).jGallery();
+} );

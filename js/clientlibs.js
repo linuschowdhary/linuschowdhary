5e55c66018837dca01eb1437f66c90df4ac730f8
Sunil Chowdhary <sunil_chowdhary@outlook.com>
2016-06-27 01:04:36 +0100

    2more pages

--- js/clientlibs.js
+++ js/clientlibs.js
@@ -1,5 +1,10 @@
 $( function() {
   $("ul.nav li.ajaxload").click(function() {
+    if(!$(this).hasClass("active"))
+    {$("ul.nav li").removeClass("active");
+      $(this).addClass("active");
+    }
+
   $.get($(this).children("a").attr("data-load"), function(data) {
     $('div.content').fadeOut('slow', function() {
       $("div.content").html(data);
